5d085f726c8549a90c6f4090d4d479bba5b440e8
sabby <sabby.6@outlook.com>
2020-12-07 13:17:14 +0530

    antiflood: support tban, tmute
    note: untested

--- sophie_bot/modules/antiflood.py
+++ sophie_bot/modules/antiflood.py
@@ -20,6 +20,7 @@ import pickle
 from dataclasses import dataclass
 from typing import Optional
 
+from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.dispatcher.handler import CancelHandler
 from aiogram.dispatcher.middlewares import BaseMiddleware
@@ -34,7 +35,7 @@ from sophie_bot import dp
 from sophie_bot.decorator import register
 from sophie_bot.modules.utils.connections import chat_connection
 from sophie_bot.modules.utils.language import get_strings_dec, get_strings
-from sophie_bot.modules.utils.message import convert_time, get_args, need_args_dec
+from sophie_bot.modules.utils.message import convert_time, get_args, need_args_dec, InvalidTimeUnit
 from sophie_bot.modules.utils.restrictions import ban_user, kick_user, mute_user
 from sophie_bot.modules.utils.user_details import is_user_admin, get_user_link
 from sophie_bot.services.mongo import db
@@ -47,6 +48,10 @@ cancel_state = CallbackData('cancel_state', 'user_id')
 
 class AntiFloodConfigState(StatesGroup):
     expiration_proc = State()
+
+
+class AntiFloodActionState(StatesGroup):
+    set_time_proc = State()
 
 
 @dataclass
@@ -127,6 +132,13 @@ class AntifloodEnforcer(BaseMiddleware):
             return await kick_user(message.chat.id, message.from_user.id)
         elif action == 'mute':
             return await mute_user(message.chat.id, message.from_user.id)
+        elif action.startswith('t'):
+            time = database.get('time', None)
+            if not time: return False
+            if action == "tmute":
+                return await mute_user(message.chat.id, message.from_user.id, until_date=convert_time(time))
+            elif action == 'tban':
+                return await ban_user(message.chat.id, message.from_user.id, until_date=convert_time(time))
         else:
             return False
 
@@ -238,9 +250,14 @@ async def antiflood(message: Message, chat: dict, strings: dict):
 @chat_connection(admin=True)
 @get_strings_dec('antiflood')
 async def setfloodaction(message: Message, chat: dict, strings: dict):
-    SUPPORTED_ACTIONS = ['kick', 'ban', 'mute']  # noqa
+    SUPPORTED_ACTIONS = ['kick', 'ban', 'mute', 'tmute', 'tban']  # noqa
     if (action := message.get_args().lower()) not in SUPPORTED_ACTIONS:
         return await message.reply(strings['invalid_args'].format(supported_actions=", ".join(SUPPORTED_ACTIONS)))
+
+    if action.startswith('t'):
+        await message.reply(strings['send_time'], allow_sending_without_reply=True)
+        redis.set(f"floodactionstate:{chat['chat_id']}", action)
+        return await AntiFloodActionState.set_time_proc.set()
 
     await db.antiflood.update_one(
         {"chat_id": chat['chat_id']},
@@ -253,6 +270,31 @@ async def setfloodaction(message: Message, chat: dict, strings: dict):
             action=action
         )
     )
+
+
+@register(state=AntiFloodActionState.set_time_proc, user_can_restrict_members=True, allow_kwargs=True)
+@chat_connection(admin=True)
+@get_strings_dec('antiflood')
+async def set_time_config(message: Message, chat: dict, strings: dict, state: FSMContext, **_):
+    if not (action := redis.get(f"floodactionstate:{chat['chat_id']}")):
+        await message.reply(strings['setup_corrupted'], allow_sending_without_reply=True)
+        return await state.finish()
+    try:
+        parsed_time = convert_time(time := message.text.lower())  # just call for making sure its valid
+    except (TypeError, ValueError, InvalidTimeUnit):
+        await message.reply(strings['invalid_time'])
+    else:
+        await db.antiflood.update_one(
+            {"chat_id": chat['chat_id']},
+            {"$set": {"action": action, "time": time}},
+            upsert=True
+        )
+        await get_data.reset_cache(chat['chat_id'])
+        text = strings['setfloodaction_success'].format(action=action)
+        text += f" ({format_timedelta(parsed_time, locale=strings['language_info']['babel'])})"
+        await message.reply(text, allow_sending_without_reply=True)
+    finally:
+        await state.finish()
 
 
 async def __before_serving__(_):
